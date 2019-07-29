2fc9ad10c255f2ea31d3bfc576ee0f3123b36ac6
Cameron Kelley <cameron.kelley@emerson.com>
2019-07-29 18:58:54 -0500

    Scaffolding for recipes

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ class App extends React.Component {
         return (
             <BrowserRouter>
                 <Route exact path="/" component={Home} />
-                <Route path="/page1" component={Page1} />
-                <Route path="/page2" component={Page2} />
+                <Route path="/recipes" component={Recipes} />
+                <Route path="/sessions" component={Sessions} />
             </BrowserRouter>
         );
     }
@@ -19,8 +19,8 @@ class Home extends React.Component {
         return (
             <div>
                 <div className='home-buttons'>
-                    <Link to="/page1">Page1</Link>
-                    <Link to="/page2">Page2</Link>
+                    <Link to="/recipes">Recipes</Link>
+                    <Link to="/sessions">Sessions</Link>
                 </div>
                 <div className='home-content'>Home page visuals</div>   
             </div>
@@ -28,15 +28,54 @@ class Home extends React.Component {
     }
 }
 
-class Page1 extends React.Component {
+interface IRecipeProps {
+    recipeId: number
+}
+
+interface IRecipeState {
+    recipeId: number,
+    isBeingEdited: boolean
+}
+
+class Recipe extends React.Component<IRecipeProps, IRecipeState> {
+    constructor(props: IRecipeProps) {
+        super(props)
+        this.state = {recipeId: props.recipeId, isBeingEdited: false };
+    }
+
     render() {
-        return (<span>Page1</span>);
+        return (
+            <div>{this.state.recipeId}</div>
+        );
     }
 }
 
-class Page2 extends React.Component {
+interface IRecipesState {
+    recipes: number[]
+}
+
+class Recipes extends React.Component<any, IRecipesState> {
+    constructor(props: Readonly<{}>) {
+        super(props)
+        this.state = {recipes: [1, 2, 3, 4, 5]};
+    }
+
     render() {
-        return (<span>Page2</span>)
+        const recipeList = this.state.recipes.map((recipe) => <li key={recipe}><Recipe recipeId={recipe}/></li>);
+
+        return (
+            <div>
+                <button className='new-recipe-button'>Create New Recipe</button>
+                <textarea className='recipe-filter' />
+                <ul>{recipeList}</ul>
+            </div>
+        );
+    }
+}
+
+class Sessions extends React.Component {
+    render() {
+        return (<span>Sessions</span>)
     }
 }
 
