7a3eb8e31773edd2e324a8109a232bcbb9d7f9bc
Cameron Kelley <cameron@cameronkelley.net>
2019-07-30 20:15:39 -0500

    Add typescript usage of Highcharts to home component

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,44 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Highcharts, { Chart, Series, ChartOptions, Options, SeriesLineOptions } from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 
 import { Recipes } from './Recipes'
 import { Sessions } from './Sessions'
 
 import './index.css';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+
+
+const homeChartOptions : Options = {
+    chart : {
+        type: 'line',
+        events: {
+            load: function () {
+                var randSeries = this.series[0]
+                var startTime = (new Date()).getTime();
+                setInterval(() => {randSeries.addPoint([(new Date()).getTime() - startTime, Math.random()], true, false)}, 1000);
+            }
+        },
+        zoomType: 'x'
+    },
+    credits: {
+        enabled: false
+    },
+    title: {
+        text: 'Random Data'
+    },
+    xAxis: {
+        type: 'datetime',
+        labels: {
+            format: "{value:%H:%M:%S}"
+        }
+    },
+    series: [{
+        type: 'line',
+    } as SeriesLineOptions]
+}
 
 function Home(props : any) {
     return (
@@ -16,19 +47,17 @@ function Home(props : any) {
                 <Link to="/recipes">Recipes</Link>
                 <Link to="/sessions">Sessions</Link>
             </div>
-            <div className='home-content'>Home page visuals</div>   
+            <div className='home-content'>
+                <HighchartsReact highcharts={Highcharts} options={homeChartOptions} />    
+            </div>   
         </div>
     );
 }
 
-
-
-function Root(props : Readonly<{}>) {
-    return (
-        <BrowserRouter>
-            <Route exact path="/" component={Home} />
-            <Route path="/recipes" component={Recipes} />
-            <Route path="/sessions" component={Sessions} />
-        </BrowserRouter>
-    );
-}
+ReactDOM.render((
+    <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route path="/recipes" component={Recipes} />
+        <Route path="/sessions" component={Sessions} />
+    </BrowserRouter>
+), document.getElementById('root'));
